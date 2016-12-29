61332e234b3cb81053f8a4417a9e11229f0843a3
CrowCrowzard <crowizard@gmail.com>
2016-12-29 23:36:32 +0900

    slack api test

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# coding: UTF-8
+
+import requests
+import key
+
+payload = {
+    "token" : key.token,
+    "pretty" : "1"
+}
+
+url = "https://slack.com/api/auth.test"
+
+res = requests.post(url, params=payload)
+
+print (res.text)
